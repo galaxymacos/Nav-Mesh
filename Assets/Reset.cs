6fed509cda422eadc29132419ec4ff184f02dd98
galaxymacos <galaxymacos@gmail.com>
2018-12-10 15:47:03 -0500

    Initial commit

---
+++ Assets/Reset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour {
+	
+	/// <summary>
+	/// If enemy catches you, restart the game
+	/// </summary>
+	/// <param name="other"></param>
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "You") {
+			SceneManager.LoadScene("SampleScene");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
